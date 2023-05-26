da238321982688240c9b8d4afcb5564cd332298b
Silvia Neves <siltatiana.sn@gmail.com>
2023-05-26 16:23:06 +0100

    Cart Funcionality

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,30 @@
 import Input from "../../UI/Input";
+import {useRef, useState} from "react";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  //Why do we use useRef here? Because we want to access the value of the input element.
+  // We can't use useState because we don't want to re - render the component when the value changes.
+  //We just want to access the value of the input element when the form is submitted.
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value; // This is a string
+    const enteredAmountNumber = +enteredAmount; // This is a number
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,13 +1,14 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   // {...props.input} is a special syntax that will add all the key-value pairs in props.input as props to the element we're creating.
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
