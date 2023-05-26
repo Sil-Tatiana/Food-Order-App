b7e60801a39dc7da5b983e336d3523d43805c329
Silvia Neves <siltatiana.sn@gmail.com>
2023-05-26 17:04:38 +0100

    useEffect, finishes the app

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,16 +2,32 @@ import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import { useContext } from "react";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    // in here we are adding a new item to the cart by using the addItem function from the cartContext object and passing in the item object as an argument to the addItem function
+    // by destructuring the item object and adding a new key value pair to the item object
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
